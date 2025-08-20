1d154e6a1e8390049d453c2dc3727f22b84d8cfb
TkhrIwsk <taka_0119volleyball@yahoo.co.jp>
2025-08-20 21:09:25 +0900

    各変数の定義を追加

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,6 +1,11 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const checkbox = document.getElementById("check");
+  const text = document.getElementById("text");
   if(checkbox.checked){
-    text.style.changeBackgroundColor="yellow";
+    text.style.backgroundColor="red";
+  }
+  else{
+    text.backgroundColor="white"; 
   }
 }
